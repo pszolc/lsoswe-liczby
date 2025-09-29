bc3ff879b59fb99df49818f8197da1930e408ed2
pszolc <pszolc957@gmail.com>
2025-09-29 16:08:53 +0200

    losowe liczby

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    //wyllsuj 10 liczb z zakresu od 1 do 20
+        //tablica musi miec rozmiar, ktory nie moze
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int [] tablicaLiczbLosowych = new int[10];
+        Random random = new Random();
+        for (int i =0; i <tablicaLiczbLosowych.length; i++){
+            tablicaLiczbLosowych[i] = random.nextInt(20) +1;
+
+        }
+        int [] tablicaLosowych1 = losujliczbydoTablicy((ileliczb));
+
+        wypisz(tablicaLosowych1);
+        //wypisz wylosowane liczny
+        System.out.println("wypisywanie tablicy");
+        for(int elementTablicy:tablicaLiczbLosowych){
+            System.out.println(elementTablicy+ ", ");
         }
     }
+    private static  int[] losujliczbydoTablicy(int ileLiczb){
+        int [] tablicaLiczbLosowych = new int[10];
+        Random random = new Random();
+        for (int i =0; i <tablicaLiczbLosowych.length; i++){
+            tablicaLiczbLosowych[i] = random.nextInt(20) +1;
+
+        }
+        return  tablicaLiczbLosowych;
+    }
+
 }
